9245d78b484ebd4b136374bda5e1fc8f8c814f31
Ryo Miyake <ryo.studiom@gmail.com>
2017-01-24 08:39:22 +0900

    Prepare setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='garoonbot',
+    packages=['garoonbot'],
+    version='0.1.0',
+    author='Ryo Miyake',
+    author_email='ryo.studiom@gmail.com',
+    description="API client for Cybozu Garoon",
+    url='http://github.com/nekoya/garoonbot',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: Groupware',
+    ],
+)
